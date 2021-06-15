b5117739ef21339f24713f6293e284a1d519740e
AMRUT22 <85402294+AMRUT22@users.noreply.github.com>
2021-06-15 18:47:24 +0530

    Add files via upload

---
+++ range.py
@@ -0,0 +1,9 @@
+mylist1=[12,-7,5,64,-14]
+mylist2=[12,14,-95,3]
+for  num in mylist1:
+   if num >=0:
+    print(num ,end = " ")
+print("\n")
+for  num1 in mylist2:
+   if num1 >=0:
+    print(num1 ,end = " ")
